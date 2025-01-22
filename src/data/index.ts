16008b0fcd7156444f7c5c6d45f442f96412d1e5
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-22 17:11:44 -0300

    Agregando las monedas para cotizar y primeros types

---
+++ src/data/index.ts
@@ -0,0 +1,9 @@
+import {Currency} from "../types"
+
+export const currencies : Currency[] = [
+    { code: 'CLP', name: 'Peso Chileno'},
+    { code: 'USD', name: 'Dolar de Estados Unidos'},
+    { code: 'MXN', name: 'Peso Mexicano'},
+    { code: 'EUR', name: 'Euro'},
+    { code: 'GBP', name: 'Libra Esterlina'},
+]
