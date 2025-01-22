257a47f87a72dc84e3fb0d999934c5904b893d43
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-22 20:22:54 -0300

    Creando el store de zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import CryptoSearchForm from "./components/CryptoSearchForm";
+import {useEffect} from "react"
+import CryptoSearchForm from "./components/CryptoSearchForm"
+import {useCryptoStore} from "./store"
 
 export default function App() {
+	const fetchCryptos = useCryptoStore(state => state.fetchCryptos)
+	useEffect(() => {
+		fetchCryptos()
+	}, [])
 	return (
 		<div className="container">
 			<h1 className="app-title">
