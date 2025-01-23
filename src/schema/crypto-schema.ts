869bd5ccfc8ce31ac297422beff5069cca1f12d7
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-22 21:03:28 -0300

    Validando la respuesta de la API con Zod

--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -5,3 +5,11 @@ export const CurrencySchema = z.object({
     name: z.string()
 })
 
+export const CryptoCurrencyResponseSchema = z.object({
+    CoinInfo: z.object({
+        FullName: z.string(),
+        Name: z.string()
+    })
+})
+
+export const CryptoCurrenciesResponseSchema = z.array(CryptoCurrencyResponseSchema)
