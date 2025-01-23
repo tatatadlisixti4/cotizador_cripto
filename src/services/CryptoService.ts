7ff25ef66ecf5a9e90ec729ae22733ba80f8e233
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-23 17:49:10 -0300

    Validando la respuesta de la api con zod

--- src/services/CryptoService.ts
+++ src/services/CryptoService.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import {CryptoCurrenciesResponseSchema} from "../schema/crypto-schema" 
+import {CryptoCurrenciesResponseSchema, CryptoPriceSchema} from "../schema/crypto-schema" 
 import {Pair} from "../types"
 
 export async function getCryptos() {
@@ -14,6 +14,8 @@ export async function getCryptos() {
 export async function fetchCurrentCryptoPrice(pair: Pair) {
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.cryptoCurrency}&tsyms=${pair.currency}`
     const {data: {DISPLAY}} = await axios(url)
-    console.log(DISPLAY[pair.cryptoCurrency][pair.currency]["PRICE"])
-    
+    const result = CryptoPriceSchema.safeParse(DISPLAY[pair.cryptoCurrency][pair.currency])
+    if(result.success) {
+        return result.data
+    }
 }
--- src/store.ts
+++ src/store.ts
@@ -18,6 +18,7 @@ export const useCryptoStore = create<CryptoStore>()(devtools(set => ({
         }))
     },
     fetchData: async (pair) => {
-        await fetchCurrentCryptoPrice(pair)
+        const result = await fetchCurrentCryptoPrice(pair)
+        console.log(result)
     }
 })))
