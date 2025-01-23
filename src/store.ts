08a57cbc1240c89386622fabef4618e4f4484378
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-22 22:36:37 -0300

    Moviendo la función que obtiene la api hacia otro archivo (services/cryptoservice.ts)

--- src/store.ts
+++ src/store.ts
@@ -1,22 +1,11 @@
-import axios from "axios"
 import {devtools} from 'zustand/middleware'
 import {create} from "zustand"
-import {CryptoCurrenciesResponseSchema} from "./schema/crypto-schema"
 import {CryptoCurrency} from "./types"
+import getCryptos from "./services/CryptoService"
 
 type CryptoStore = {
     cryptoCurrencies: CryptoCurrency[],
     fetchCryptos: () => Promise<void>
-}
-
-async function getCryptos() {
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
-    const {data: {Data}} = await axios(url)
-    const result = CryptoCurrenciesResponseSchema.safeParse(Data)
-    
-    if(result.success) {
-        return result.data
-    }
 }
 
 export const useCryptoStore = create<CryptoStore>()(devtools(set => ({
