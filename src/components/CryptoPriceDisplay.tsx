9ae7ab5a5c3f72160f2571a72e61d65476c5f289
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-23 22:15:44 -0300

    Agregando CSS al resultado de la cotización

--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -3,16 +3,23 @@ import {useCryptoStore} from "../store"
 export default function CryptoPriceDisplay() {
     const result = useCryptoStore(state => state.result)
     const hasResult = useMemo(() => !Object.values(result).includes(''), [result])
-    console.log(hasResult)
     
     return (
-        <div>
+        <div className="result-wrapper ">
             {hasResult && (
                 <>
                     <h2>Cotización</h2>
                     <div className="result">
+                        <img 
+                            src={`https://cryptocompare.com/${result.IMAGEURL}`} 
+                            alt="" 
+                        />
                         <div>
                             <p>El precio es de: <span>{result.PRICE}</span></p>
+                            <p>Precio más alto del día: <span>{result.HIGHDAY}</span></p>
+                            <p>Precio más bajo del día: <span>{result.LOWDAY}</span></p>
+                            <p>Variación últimas 24 horas <span>{result.CHANGEPCT24HOUR}</span></p>
+                            <p>Última actualización: <span>{result.LASTUPDATE}</span></p>
                         </div>
                     </div>
                 </>
